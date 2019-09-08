5dcd3f2a3e94971febcfb596f32c1b895c6e3560
Joel Gaudreault <joel.gaudreault@usherbrooke.ca>
2019-09-08 15:06:03 -0400

    added the Atmel Studio Project for the board

---
+++ ATMEGA256RFR2/ATMEGA256RFR2/main.c
@@ -0,0 +1,18 @@
+/*
+ * ATMEGA256RFR2.c
+ *
+ * Created: 2019-09-08 14:53:12
+ * Author : jgaud
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
