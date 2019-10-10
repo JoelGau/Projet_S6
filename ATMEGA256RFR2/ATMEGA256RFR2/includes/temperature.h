34ba0103ab82b696c775d9300a258008e48d0086
Joel Gaudreault <joel.gaudreault22@gmail.com>
2019-10-10 15:31:24 -0400

    Added getTemperature and TWI_Init functions

---
+++ ATMEGA256RFR2/ATMEGA256RFR2/includes/temperature.h
@@ -0,0 +1,15 @@
+/*
+ * temperature.h
+ *
+ * Created: 2019-10-09 11:30:16
+ *  Author: jgaud
+ */ 
+
+
+#ifndef TEMPERATURE_H_
+#define TEMPERATURE_H_
+
+void TWI_init();
+short getTemperature();
+
+#endif /* TEMPERATURE_H_ */
--- ATMEGA256RFR2/ATMEGA256RFR2/src/main.c
+++ ATMEGA256RFR2/ATMEGA256RFR2/src/main.c
@@ -4,24 +4,30 @@
 
 
 #include "../includes/miscellaneous.h"
+#include "../includes/temperature.h"
 
 #include <stdint-gcc.h>
 #include <avr/interrupt.h>
+#include <stdio.h>
 
 int main(void)
 {
-	uint8_t i;
 	Board_Init();
 	Timer_Init();
+	TWI_init();
 	
 	sei(); // Enable global interrupts
-	
+	short temp=5;
+	short i;
     while(1)
     {
-        LED_Toggle();
 		// Wait 0.5 seconds
+		if (temp != getTemperature()){
+			temp = getTemperature();
+			LED_Toggle;
+		}
 		for(i=0; i<250; i++){
 			Delay(50000); //us
-		}
+		}		
     }
 }
