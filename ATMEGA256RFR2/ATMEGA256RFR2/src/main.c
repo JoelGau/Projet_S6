a346bba1e391ae313d51d98f9e5730a934e79aa9
Joel Gaudreault <joel.gaudreault22@gmail.com>
2019-10-12 16:57:56 -0400

    ça fonctionne calice
    en gros j'ai pris des fonctions déjà faites

--- ATMEGA256RFR2/ATMEGA256RFR2/src/main.c
+++ ATMEGA256RFR2/ATMEGA256RFR2/src/main.c
@@ -2,8 +2,10 @@
  * 
  */ 
 #include "../includes/temperature.h"
+#include "../includes/at30ts.h"
 
 #include <stdint-gcc.h>
+#include <delay.h>
 #include <sysclk.h>
 #include <avr/interrupt.h>
 #include <stdio.h>
@@ -15,15 +17,16 @@ int main(void)
 	sysclk_init();
 	board_init();
 	TWI_init();
-	//AT30TSE758_init();
 	
 	sei(); // Enable global interrupts
-	short temp=5;
+	uint16_t temp=5;
+	sensor_data_t data;
     while(1)
     {
+		//read_temperature(&data);
 		if (temp != getTemperature()){
 			temp = getTemperature();
-			LED_Toggle(0);
-		}	
+		}
+		delay_ms(250);
     }
 }
--- ATMEGA256RFR2/ATMEGA256RFR2/src/temperature.c
+++ ATMEGA256RFR2/ATMEGA256RFR2/src/temperature.c
@@ -6,6 +6,8 @@
  */ 
 
  #include "../includes/temperature.h"
+ #include "../includes/at30ts.h"
+
  #include <twi_master.h>
  #include <twi_megarf.h>
  #include <twi_slave.h>
@@ -42,48 +44,11 @@ const uint8_t AT30TSE758_TempRead [SLAVE_MEM_ADDR_LENGTH] = {
 	/* Initialize the TWI master driver. */
 	twi_master_init(TWI_BULLSHIT, &opt);			
 	delay_ms(100);					// waits 100 ms
- }
- 
- void AT30TSE758_init() {
- 	// Sets AT30TSE758
- 	const uint8_t command[] = {0x60,0x00}; // 12 bits resolution temperature
-
-	twi_package_t packet_write = {
-		.addr[0]      =  AT30TSE758_TempWrite[0],		/* TWI slave memory address data */
-		.addr_length  = (uint8_t)SLAVE_MEM_ADDR_LENGTH, /* TWI slave memory address data size */
-		.chip         = SLAVE_BUS_ADDR,					/* TWI slave bus address */
-		.buffer       = (void *)command,				/* transfer data source buffer */
-		.length       = sizeof(command)					/* transfer data size (bytes) */
-	};
-	delay_ms(100);					// waits 100 ms
- 	/*twi_package_t packet_write = {
-	 	.addr         = 0x4B,					// TWI slave memory address data
-	 	.addr_length  = 1,						// TWI slave memory address data size
-	 	.chip         = SLAVE_BUS_ADDR,			// TWI slave bus address
-	 	.buffer       = (void *)command,		// transfer data source buffer
-	 	.length       = 2						// transfer data size (bytes)
- 	};*/
- 	while (twi_master_write(TWI_BULLSHIT, &packet_write) != TWI_SUCCESS);
+	attach_device(0x96,AT30TSE758);
  }
 
  short getTemperature() {
-	uint8_t data_received[2];
-	twi_package_t packet_read = {
-		.addr[0]      =  AT30TSE758_TempRead[0],		/* TWI slave memory address data */
-		.addr_length  = (uint8_t)SLAVE_MEM_ADDR_LENGTH, /* TWI slave memory address data size */
-		.chip         = SLAVE_BUS_ADDR,					/* TWI slave bus address */
-		.buffer       = (void *)data_received,			/* transfer data destination buffer */
-		.length       = sizeof(data_received)			/* transfer data size (bytes) */
-	};
-	/*twi_package_t packet_read = {
-		 .addr         = 0x4B,					// TWI slave memory address data
-		 .addr_length  = 1,						// TWI slave memory address data size
-		 .chip         = SLAVE_BUS_ADDR,		// TWI slave bus address
-		 .buffer       = data_received,			// transfer data destination buffer
-		 .length       = 2						// transfer data size (bytes)
-	};*/
-	while (twi_master_read(TWI_BULLSHIT, &packet_read) != TWI_SUCCESS);
-	uint16_t temperature = data_received[0] & data_received[1];
-	temperature = temperature >> 4;
-	return temperature;
+	sensor_data_t data;
+	read_temperature(&data);
+	return data.temperature.raw_value;
  }
