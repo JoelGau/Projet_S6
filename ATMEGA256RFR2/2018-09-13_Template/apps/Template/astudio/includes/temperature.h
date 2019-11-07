0cbe454acb879ffd1036f888af06ce5f9a2e7d44
Charles-Frédérick Gauthier <gauc2329@usherbrooke.ca>
2019-11-07 11:21:23 -0500

    Form management V1
    Also added a new standard temperature acquisition which returns a float.

--- ATMEGA256RFR2/2018-09-13_Template/apps/Template/astudio/includes/temperature.h
+++ ATMEGA256RFR2/2018-09-13_Template/apps/Template/astudio/includes/temperature.h
@@ -13,5 +13,6 @@ void TWI_init();
 void AT30TSE758_init();
 short getTemperatureRaw();
 void getTemperatureCelsius(short* celsius);
+void getTemperatureCelsiusStd(float* celsius);
 
 #endif /* TEMPERATURE_H_ */
